669165892f45e1bcbb355ecc77b4f3737c89ead0
Wen-Tien Chang <ihower@gmail.com>
2014-11-03 10:36:17 +0800

    Add categories

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -31,7 +31,7 @@ class TopicsController < ApplicationController
   protected
 
   def topic_params
-    params.require(:topic).permit(:title, :content)
+    params.require(:topic).permit(:title, :content, :category_ids => [])
   end
 
 end
